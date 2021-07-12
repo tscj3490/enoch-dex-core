387e6777c3c08ac3233da8f26967e94518489a97
sareen_01 <sanya16sareen@gmail.com>
2021-07-12 13:55:21 +0530

    Increased the amount for minting tokens

--- contracts/Token1.sol
+++ contracts/Token1.sol
@@ -1,4 +1,4 @@
-pragma solidity =0.5.16;
+pragma solidity >= 0.5.16;
 
 import '@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol';
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
@@ -6,8 +6,13 @@ import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 
 contract Token1 is ERC20Detailed, ERC20 {
     constructor() ERC20Detailed('Token1','TK1',18) public{
-        _mint(msg.sender, 10000 * (10 ** 18));
+        _mint(msg.sender, 1000000000000 * (10 ** 18));
     }
 }
+
+
+
+
+
 
 // msg.sender, 10000 * (10 ** uint256(decimals()))
