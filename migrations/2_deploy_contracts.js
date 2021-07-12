524ed4d85153168ceb46e2d22dc335a067cd137a
sareen_01 <sanya16sareen@gmail.com>
2021-07-12 13:56:27 +0530

    Getting the HASH from getCreationCode() -> For adding Liquidity

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -5,6 +5,7 @@ const Token2 = artifacts.require("Token2.sol");
 const BN = require("bn.js");
 
 module.exports = async function (deployer, network, addresses) {
+
   await deployer.deploy(Factory, addresses[0]); //send transaction
   const factory = await Factory.deployed(); //gets mined
 
@@ -21,19 +22,19 @@ module.exports = async function (deployer, network, addresses) {
       const token2 = await Token2.deployed();
       token1Address = token1.address;
       token2Address = token2.address;
-      
-      await token1.transfer("0xb9B62EaE8BaDe74880C5B2C622a3ccAeF28769D6",100);
-      // approve Router address
-      await token1.approve("0xEb83b4a419F2e9e70321897A438f67C0275ba349",10)
-      await token2.approve("0xEb83b4a419F2e9e70321897A438f67C0275ba349",10)
 
-
+      console.log("Token 1 address", token1Address)
+      console.log("Token 2 address", token2Address)
+      console.log("Factory address", factory.address)
+    
       await factory.createPair(token1Address, token2Address);
 
+      console.log(await factory.getCreationCode())
       
 
   }
 };
+// .getCreationCode().call()
 
 
 
