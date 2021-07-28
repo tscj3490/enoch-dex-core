2eec05cf78eb3c7fbf9cf3e046ec5880d541191b
sareen_01 <sanya16sareen@gmail.com>
2021-07-28 11:30:55 +0530

    Deployed WETH

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,6 +2,7 @@ const Factory = artifacts.require("EnochV2Factory.sol");
 const Token1 = artifacts.require("Token1.sol");
 const Token2 = artifacts.require("Token2.sol");
 const EnochV2Pair = artifacts.require("EnochV2ERC20.sol");
+const WETH = artifacts.require("WETH.sol");
 const BN = require("bn.js");
 
 module.exports = async function (deployer, network, addresses) {
@@ -21,8 +22,10 @@ module.exports = async function (deployer, network, addresses) {
 
       await deployer.deploy(Token1);
       await deployer.deploy(Token2);
+      await deployer.deploy(WETH);
       const token1 = await Token1.deployed();
       const token2 = await Token2.deployed();
+      const Weth = await WETH.deployed();
       token1Address = token1.address;
       token2Address = token2.address;
 
@@ -30,10 +33,10 @@ module.exports = async function (deployer, network, addresses) {
       console.log("Token 1 address", token1Address)
       console.log("Token 2 address", token2Address)
       console.log("Factory address", factory.address)
-    
-     await factory.createPair(token1Address, token2Address);
-      console.log("Pair address: ", await factory.getPair(token1Address, token2Address));
-      console.log("Creation Code", await factory.getCreationCode())
+      console.log("WETH address:", Weth.address);
+    //  await factory.createPair(token1Address, token2Address);
+    //   console.log("Pair address: ", await factory.getPair(token1Address, token2Address));
+    //   console.log("Creation Code", await factory.getCreationCode())
       // console.log("Enoch V2 Pair Contract address for ERC20 Functionailities:", EnochV2pair.address);
 
      
