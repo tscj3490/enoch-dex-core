8642432d586b9edf1eb722a9098def67dc0db1c5
Dhruv Godambe <dhruvrockstone@gmail.com>
2021-09-25 17:46:58 +0530

    deleted unwanted

--- contracts/test/ERC20.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity =0.5.16;
-
-import '../EnochV2ERC20.sol';
-
-contract ERC20 is EnochV2ERC20 {
-    constructor(uint _totalSupply) public {
-        _mint(msg.sender, _totalSupply);
-    }
-}
